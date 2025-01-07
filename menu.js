918050f659ae925b383cb2ebd6a8e9f40b9904f4
pavdwest <pavdwesthuizen@gmail.com>
2025-01-07 13:22:34 +0100

    sandbox

---
+++ menu.js
@@ -0,0 +1,7 @@
+document.write("<div id=\"menu\">");
+document.write("<ul>");
+document.write("<li class='active'><a href='index.html'><span>Home<\/span><\/a><\/li>");
+document.write("<li><a href='about.html'><span>About<\/span><\/a><\/li>");
+document.write("<li><a href='contact.html'><span>Contact<\/span><\/a><\/li>");
+document.write("<\/ul>");
+document.write("<\/div>");
